5c96a183b600364d424c10a37b46f6fb4c69e39d
Nomonkhon <sobitnomonxon@gmail.com>
2025-05-07 10:20:54 +0300

    New update

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+let board;
+let game;
 function showDifficulty() {
   document.getElementById("startScreen").style.display = "none";
   document.getElementById("difficultyScreen").style.display = "block";
@@ -8,7 +10,21 @@ function startGame(level) {
   document.getElementById("difficultyScreen").style.display = "none";
   document.getElementById("game").style.display = "block";
   startTimer();
-  document.getElementById("board").innerHTML = "<p>Здесь будет шахматная доска (" + level + ")</p>";
+
+  game = new Chess();
+  board = Chessboard('board', {
+    draggable: true,
+    position: 'start',
+    onDrop: function (source, target) {
+      let move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q'
+      });
+      if (move === null) return 'snapback';
+      window.setTimeout(makeBestMove, 250);
+    }
+  });
 }
 
 function startTimer() {
@@ -24,4 +40,19 @@ function startTimer() {
       alert("Время вышло! Вы проиграли чемпиону мира. Это тоже достижение!");
     }
   }, 1000);
+}
+
+function makeBestMove() {
+  let possibleMoves = game.moves();
+  if (game.game_over() || possibleMoves.length === 0) {
+    if (game.in_checkmate()) {
+      alert("Поздравляю, теперь вы чемпион мира!");
+    } else {
+      alert("Ничья!");
+    }
+    return;
+  }
+  let move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+  game.move(move);
+  board.position(game.fen());
 }
